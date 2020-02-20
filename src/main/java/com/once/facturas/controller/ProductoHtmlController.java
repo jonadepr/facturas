c0169a8a57055c3b1aa8d8667c151543bce4f1be
jonadepr <eljosele4@gmail.com>
2020-02-20 16:26:41 +0100

    Crear y update productos listo

--- src/main/java/com/once/facturas/controller/ProductoHtmlController.java
+++ src/main/java/com/once/facturas/controller/ProductoHtmlController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -20,7 +22,7 @@ public class ProductoHtmlController {
      * GET /productos/ - Nos devolverá una lista de productos OK
      * GET /productos/1/ - Nos devolverá el producto con id = 1 (READ) OK
      * DELETE /productos/1/ - Nos borrará el producto número 1 (DELETE) OK
-     * PUT /productos/1/ - Nos modificará el producto número 1 (UPDATE) 
+     * PUT /productos/1/ - Nos modificará el producto número 1 (UPDATE) OK
      * POST /productos/ - Nos crea un nuevo producto (CREATE)
      * Y luego está el PATCH: PATCH /productos/1/ nos permite modificar
      * una factura pero de la forma "añade 2 euros a la factura".
@@ -54,12 +56,56 @@ public class ProductoHtmlController {
         return modelAndView;
     }
 
+    private Long idActualizar = Long.valueOf("0");
+
     @GetMapping("/{id}/update/")
     public ModelAndView updateProductoGet(@PathVariable("id") Long id){
         ModelAndView modelAndView=new ModelAndView("updateProducto");
         Producto elProducto = pr.findById(id).get();
         modelAndView.addObject("productos", elProducto);
+        idActualizar = id;
         return modelAndView;
     }
+
+
+    @PostMapping("/update")
+    public ModelAndView clientePost(
+        @RequestParam("descripcion") String descripcion,
+        @RequestParam("fabricante") String fabricante,
+        @RequestParam("precio") Float precio
+    ){
+        ModelAndView modelAndView=new ModelAndView("updateProducto");
+        System.out.println(idActualizar);
+        Producto actual = pr.findById(idActualizar).get();
+        actual.setDescripcion(descripcion);
+        actual.setFabricante(fabricante);
+        actual.setPrecio(precio);
+        pr.save(actual);
+        return modelAndView;
+    }
+
+    @GetMapping("/crearproducto")
+    public ModelAndView crearProducto() {
+        ModelAndView modelAndView = new ModelAndView("crearProducto");
+        modelAndView.addObject("mensaje", "");
+        return modelAndView;
+    }
+
+    @PostMapping("/crearProducto")
+    public ModelAndView insertarHTMLPost(
+        @RequestParam("fabricante") String fabricante,
+        @RequestParam("descripcion") String descripcion, 
+        @RequestParam("precio") Float precio) {
+
+        ModelAndView modelAndView = new ModelAndView("crearProducto");
+        Producto nuevo =  new Producto();
+        nuevo.setDescripcion(descripcion);
+        nuevo.setFabricante(fabricante);
+        nuevo.setPrecio(precio);
+        nuevo = pr.save(nuevo);
+        modelAndView.addObject("mensaje", nuevo);
+        return modelAndView;
+    }
+
 
 }
