edfa3bd185667966cde883baf2c9d28c40e92a22
jonadepr <eljosele4@gmail.com>
2020-02-20 12:48:14 +0100

    ProductoApiController completado

--- src/main/java/com/once/facturas/controller/ProductoApiController.java
+++ src/main/java/com/once/facturas/controller/ProductoApiController.java
@@ -6,9 +6,12 @@ import com.once.facturas.model.Producto;
 import com.once.facturas.model.ProductoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,14 +25,25 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController // Devuelvo siempre algo convertido en json
-@RequestMapping(value = "/productos/") // Mapeo de este controlador
-class ProductoController {
+@RequestMapping(value = "/api/productos/") // Mapeo de este controlador
+class ProductoApiController {
     /**
      * Escucha en las siguientes rutas
-     * GET /productos/
-     * GET /productos/{id}
-     * GET /productos/hello
-     * GET /productos/count
+     * GET /api/productos/
+     * GET /api/productos/{id}
+     * GET /api/productos/hello
+     * GET /api/productos/count
+     * 
+     * GET /api/facturas/ - Nos devolverá una lista de facturas OK
+     * GET /api/facturas/1/ - Nos devolverá la factura número 1 (READ) OK 
+     * DELETE /api/facturas/1/ - Nos borrará la factura número 1 (DELETE) OK
+     * PUT /api/facturas/1/ - Nos modificará la factura número 1 (UPDATE) OK
+     * POST /api/facturas/ - Nos crea una nueva factura (CREATE) OK
+     * Y luego está el PATCH: PATCH /api/facturas/1/ nos permite modificar OK
+     * una factura pero de la forma "añade 2 euros a la factura".
+     * 
+     * 
+     * 
      * 
     */
     @Autowired
@@ -60,6 +74,46 @@ class ProductoController {
     ) {
         Producto p = pr.save(producto);
         return p;
+    }
+
+    // DELETE /api/facturas/{id}/
+    @DeleteMapping("/{id}/")
+    public Producto eliminarProducto(@PathVariable("id") Long id){
+        try{
+            pr.deleteById(id);
+        }catch(IllegalArgumentException e){
+            e = new IllegalArgumentException("No se ha encontrado el producto a eliminar");
+            System.out.println(e.getMessage());
+            return null;
+        }
+        return null;
+    }
+
+    // PUT /api/facturas/1/ - Nos modificará la factura número 1 (UPDATE) 
+    @PutMapping("/{id}/")
+    public Producto modificarProducto(
+        @PathVariable("id") Long id, 
+        @RequestBody Producto producto){
+        Producto aModif = pr.findById(id).get();
+        aModif.setDescripcion(producto.getDescripcion());
+        aModif.setFabricante(producto.getFabricante());
+        aModif.setPrecio(producto.getPrecio());
+        return pr.save(aModif);
+    }
+
+    /**
+     * Y luego está el PATCH: PATCH /api/facturas/1/ nos permite modificar 
+     * una factura pero de la forma "añade 2 euros a la factura".
+     * 
+     */
+    @PatchMapping("/{id}/")
+    public Producto modificarProductoAnyadeEuros(
+        @PathVariable("id") Long id,
+        @RequestBody Producto producto){
+            Producto aModif = pr.findById(id).get();
+            aModif.setPrecio(aModif.getPrecio() + producto.getPrecio());
+            pr.save(aModif);
+            return null;
     }
 
 
---
+++ src/main/java/com/once/facturas/controller/ProductoHtmlController.java
@@ -0,0 +1,10 @@
+package com.once.facturas.controller;
+
+
+
+
+
+
+
+
+@Controller
